d3cb82bcfc2c1262ad6c3ef155e6711ea25844a7
Elmer <elmer.go@outlook.com>
2021-08-23 22:13:58 -0400

    Algoritmos greedy terminados

--- algorithms/greedy.py
+++ algorithms/greedy.py
@@ -1,3 +1,4 @@
+from constants import MAX_WEIGHT
 from models.item import Item
 
 # Todos los algoritmos retornaran una lista de items
@@ -7,8 +8,20 @@ def h1_menor_peso(items: list) -> list:
     Esta heuristica va almacenando los items priorizando los que
     tienen menor peso hasta llenar el peso maximo
     '''
-    
-    return []
+
+    sorted_items: list[Item] = sorted(items, key=lambda i: i.weight)
+
+    result = []
+    sum = 0
+
+    for item in sorted_items:
+        if sum + item.weight > MAX_WEIGHT:
+            break
+
+        sum += item.weight
+        result.append(item)
+
+    return result
 
 def h2_mayor_valor(items: list) -> list:
     '''
@@ -16,7 +29,19 @@ def h2_mayor_valor(items: list) -> list:
     tienen mayor valor hasta llenar el peso maximo
     '''
     
-    return []
+    sorted_items: list[Item] = sorted(items, key=lambda i: i.value, reverse=True)
+
+    result = []
+    sum = 0
+
+    for item in sorted_items:
+        if sum + item.weight > MAX_WEIGHT:
+            break
+
+        sum += item.weight
+        result.append(item)
+
+    return result
 
 def h3_mayor_cociente(items: list) -> list:
     '''
@@ -24,5 +49,18 @@ def h3_mayor_cociente(items: list) -> list:
     tienen una mayor relacion valor/peso
     hasta llenar el peso maximo
     '''
-    
-    return []
+
+    cocientes = [(item.value / item.weight, item) for item in items]
+    sorted_items = sorted(cocientes, key=lambda i: i[0], reverse=True)
+
+    result = []
+    sum = 0
+
+    for cociente, item in sorted_items:
+        if sum + item.weight > MAX_WEIGHT:
+            break
+
+        sum += item.weight
+        result.append(item)
+
+    return result
